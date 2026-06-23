12acb5f82a6fb03809342ab9fba042964432798e
pvgamileira <pvgamileira@gmail.com>
2026-06-23 18:29:24 -0300

    fix: adiciona DROP POLICY IF EXISTS na migracao

--- supabase/migrations/20260623000001_fix_usuarios_rls.sql
+++ supabase/migrations/20260623000001_fix_usuarios_rls.sql
@@ -1,3 +1,4 @@
 -- Allow anonymous updates for usuarios (necessário para o Primeiro Acesso gravar a senha)
+DROP POLICY IF EXISTS "Anonymous update for usuarios" ON public.usuarios;
 CREATE POLICY "Anonymous update for usuarios" ON public.usuarios
     FOR UPDATE USING (true);
